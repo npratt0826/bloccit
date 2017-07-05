e5697b82c9bf4ef8585bfe6a8c35827528f9896c
Nick Pratt <nicholasapratt@gmail.com>
2017-07-05 20:40:09 +0000

    assignment 16: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,10 @@
    )
  end
  
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "Unique title", body: "Unique body")
+ puts "#{Post.count}"
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
