dc9d6c56bb15b88b5964456a8c5b40f2bc61d662
Nick Pratt <nicholasapratt@gmail.com>
2017-07-17 00:00:04 +0000

    assignment 27: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
   
   before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_user_moderator, except: :show
 
   def show
     @post = Post.find(params[:id])
@@ -64,8 +65,16 @@ class PostsController < ApplicationController
    def authorize_user
       post = Post.find(params[:id])
       
-      unless current_user == post.user || current_user.admin? || current_user.moderator?
+      unless current_user == post.user || current_user.admin?
         flash[:alert] = "You must be an admin to do that."
+        redirect_to [post.topic, post]
+      end
+   end
+   
+   def authorize_user_moderator
+      post = Post.find(params[:id])
+      unless current_user.moderator?
+        flash[:alert] = "You must be a moderator to do that."
         redirect_to [post.topic, post]
       end
    end
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,7 +3,7 @@ module TopicsHelper
         current_user && current_user.admin?
     end
     
-    def user_is_authorized_for_topics_update?
+    def user_is_authorized_for_topic_update?
         current_user && current_user.moderator?
     end
 end
